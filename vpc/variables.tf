a97b17f265cff7b3b5f7f3142b094368497a2701
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2024-01-25 11:46:32 +0530

    Add files via upload

---
+++ vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {}
+variable "environment" {}
+variable "business_divsion" {}
+variable "vpc_name" {}
+variable "vpc_cidr_block" {}
+variable "vpc_availability_zones" {}
+variable "vpc_public_subnets" {}
+variable "vpc_private_subnets" {}
+variable "vpc_create_database_subnet_group" {}
+variable "vpc_create_database_subnet_route_table" {}
+variable "vpc_enable_nat_gateway" {}
+variable "vpc_single_nat_gateway" {}
